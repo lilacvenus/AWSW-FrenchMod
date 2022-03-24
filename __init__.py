39ebeb3e98396749837a6be4f4bcb9d41b7c25c5
Venus <66577598+lilacvenus@users.noreply.github.com>
2022-03-24 16:16:50 -0300

    Before fixing translation nodes

---
+++ __init__.py
@@ -0,0 +1,23 @@
+﻿#Hello :) 
+from modloader import modast, modinfo
+from modloader.modclass import Mod, loadable_mod
+import jz_magmalink as ml
+
+@loadable_mod
+class AWSWMod(Mod):
+    name = "French AWSW"
+    version = "1.00"
+    author = "LilacVenus"
+    dependencies = ["MagmaLink"]
+
+    def mod_load(self):
+        c = ml.find_label("splashscreen") \
+            .search_if("persistent.lang == \"Jp\"")
+        c.branch() \
+            .search_menu().add_choice(text=u"Français {image=image/ui/lang-fr.png}".encode('utf-8'), jump="venus_ChangeToFrench")
+        c.branch_else() \
+            .search_menu().add_choice(text=u"Français {image=image/ui/lang-fr2.png}".encode('utf-8'), jump="venus_ChangeToFrench")
+        c.link_behind_from("venus_ChangeToFrench_end")
+
+    def mod_complete(self):
+        pass
---
+++ frenchAWSW.rpy
@@ -0,0 +1,4 @@
+label venus_ChangeToFrench:
+    $ renpy.change_language("frenchtl")
+
+    jump venus_ChangeToFrench_end
